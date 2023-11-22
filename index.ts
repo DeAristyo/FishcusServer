b6e84fd2ba11a180bc3bd92de26b78b8a2c5b6f5
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2023-11-22 20:08:32 +0700

    dotenv settings

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+const cors = require('cors');
 
-dotenv.config();
+const envFile =
+	process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local';
+dotenv.config({ path: envFile });
 
 const app: Express = express();
 const port = process.env.PORT;
-console.log(`Port: ${port}`);
+
+app.use(
+	cors({
+		credentials: true,
+		origin: process.env.API_URL,
+	})
+);
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Jalan Bosq');
+	res.send(`Jalan Bosq di ${envFile}`);
 });
 
 app.listen(port, () => {
-	console.log(`⚡️[Fishcus]: Server is running at http://localhost:${port}`);
+	console.log(
+		`⚡️[Fishcus]: Server is running at http://localhost:${port} ${envFile}`
+	);
 });
